ac894cff0f769d72560ba7ed94747d02ee0b08fe
dmitriy.karyakin <dmitriy.karyakin@walletone.com>
2017-08-01 17:17:49 +0500

    drum kit - 01

---
+++ 01-drum-kit/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.addEventListener('keydown', function(e)
+{
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add('active');
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e)
+{
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('active');
+}
